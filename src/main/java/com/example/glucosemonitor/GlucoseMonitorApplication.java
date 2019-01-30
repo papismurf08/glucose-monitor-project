70bca0a0876b4dcd31ec8c3e6beca476cb124857
Michael <32988594+papismurf08@users.noreply.github.com>
2019-01-29 21:28:47 -0500

    Initial commit

---
+++ src/main/java/com/example/glucosemonitor/GlucoseMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.example.glucosemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoseMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucoseMonitorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/glucosemonitor/GlucoseMonitorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.glucosemonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlucoseMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
